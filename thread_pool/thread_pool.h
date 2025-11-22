3676f6b7540ceff7e201dee936336e6123ec73f4
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2025-11-22 19:04:34 +0000

    added the usage of futures to allow keeping track of returns

--- thread_pool/thread_pool.h
+++ thread_pool/thread_pool.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <future>
 #include <thread>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace multithreads {
@@ -21,12 +24,16 @@ public:
     this->threads_.reserve(amount_of_threads);
     for (int i = 0; i < amount_of_threads; ++i) {
       auto worker = [this] {
-        while (!this->shutdown_requested_) {
+        while (!this->shutdown_requested_ ||
+               (this->shutdown_requested_ && !this->queue_.empty())) {
           std::unique_lock<std::mutex> lock(this->mutex_);
-          this->condition_variable_.wait(lock, this->shutdown_requested_ ||
-                                                   !this->queue_.empty());
-          if (this->shutdown_requested_ || this->queue_.empty())
+          this->condition_variable_.wait(lock, [this] {
+            return this->shutdown_requested_ || !this->queue_.empty();
+          });
+
+          if (this->shutdown_requested_ && this->queue_.empty()) {
             return;
+          }
 
           auto task = std::move(this->queue_.front());
           this->queue_.pop();
@@ -50,11 +57,25 @@ public:
     }
   }
 
-  template <class F> void addTask(F &&task) {
+  template <class F, typename... Args>
+  auto addTask(F &&task, Args &&...args)
+      -> std::future<std::invoke_result_t<F, Args...>> {
     std::unique_lock<std::mutex> lock(this->mutex_);
-    this->queue_.emplace(std::forward<F>(task));
+
+    auto func = std::bind(std::forward<F>(task), std::forward<Args>(args)...);
+    auto packaged_task_ptr = std::make_shared<
+        std::packaged_task<std::invoke_result_t<F, Args...>()>>(func);
+
+    auto future = packaged_task_ptr->get_future();
+
+    // wrap the task to get rid of the return value to standardize it
+    auto task_wrapper = [packaged_task_ptr] { (*packaged_task_ptr)(); };
+
+    this->queue_.emplace(std::move(task_wrapper));
     lock.unlock();
     this->condition_variable_.notify_one();
+
+    return future;
   };
 };
 } // namespace multithreads
